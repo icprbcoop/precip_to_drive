4e2ca3908621b40aea3391867aa5ba5d7e721a5b
ShyGuyPy <luke.vawter1@gmail.com>
2019-10-09 13:44:40 -0400

    initial commit

---
+++ test_drive.py
@@ -0,0 +1,8 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
+
+file1 = drive.CreateFile({'title': "Hello.txt"})
+file1.SetContentString('Hello World!')
+file1.Upload()
